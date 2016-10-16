ea242241ee97eafc6bdf5e2dcd9d0fe54382128f
tompng <tomoyapenguin@gmail.com>
2016-10-16 13:09:08 +0900

    svg fill

--- bezier.js
+++ bezier.js
@@ -4,6 +4,39 @@ function sliceLineParams(a,b){
   return [dy, -dx, -dy*a.x+dx*a.y]
 }
 function vectorMix(a,b,t){return {x: a.x*(1-t)+t*b.x, y: a.y*(1-t)+t*b.y}}
+function pathLineSlice(beziers, linecoef){
+  var outputs=[]
+  var joins=[]
+  beziers.map(function(bezier){
+    var beziers=bezierLineSlice(bezier, linecoef)
+    beziers.forEach(function(b,i){
+      outputs.push(b)
+      if(i>0)joins.push(b[0])
+      else if(b[0].x!=bezier[0].x||b[0].y!=bezier[0].y)joins.push(b[0])
+      if(i<beziers.length-1)joins.push(b[3])
+      else if(b[3].x!=bezier[3].x||b[3].y!=bezier[3].y)joins.push(b[3])
+    })
+  })
+  var starts={}
+  outputs.forEach(function(b){starts[[b[0].x,b[0].y]]=true})
+  joins.sort(function(a,b){
+    var aa=linecoef[0]*a.y-linecoef[1]*a.x
+    var bb=linecoef[0]*b.y-linecoef[1]*b.x
+    return aa>bb?1:aa<bb?-1:0
+  })
+  var jstarts=[], jends=[]
+  joins.forEach(function(j){
+    if(starts[[j.x,j.y]])jstarts.push(j)
+    else jends.push(j)
+  })
+  if(jends.length!=jstarts.length)console.error(jends.length,jstarts.length)
+  for(var i=0;i<jstarts.length;i++){
+    var p3=jstarts[i], p0=jends[i]||jends[jends.length-1]||p3
+    outputs.push([p0,vectorMix(p0,p3,1/3),vectorMix(p0,p3,2/3),p3])
+  }
+
+  return outputs
+}
 function bezierLineSlice(bezier, linecoef){
   var xcoef=linecoef[0], ycoef=linecoef[1], ccoef=linecoef[2]
   var n=100;
